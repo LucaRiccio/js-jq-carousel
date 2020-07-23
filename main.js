2b15f1fb63a77e60783c37249598d1c161b457b5
LucaRiccio <lucariccio888@gmail.com>
2020-07-23 17:54:41 +0200

    Consegna esercizio (senza bonus)

---
+++ main.js
@@ -0,0 +1,54 @@
+$(document).ready(function() {
+
+  //al click del tasto/classe prev richiamo una funzione che scorre le immagini.
+  $(".prev").click(function() {
+    prevImage();
+  })
+
+  //al click del tasto/classe next richiamo una funzione che scorre le immagini.
+  $(".next").click(function() {
+    nextImage();
+  })
+
+// ----FUNZIONI----
+
+  function nextImage(){
+
+    //Memorizzo in una variabile l'immagine attiva.
+    var activeImage = $(".images img.active");
+
+    //Memorizzo in una variabile l'icona attiva.
+    var activeCircle = $(".nav i.active")
+
+    //tolgo la classe
+    activeImage.removeClass("active");
+    activeCircle.removeClass ("active");
+
+    //metto la classe al next
+    if (activeImage.hasClass("last")){
+      $(".images img.first").addClass("active");
+      $(".nav i.first").addClass("active");
+    }else {
+      activeImage.next().addClass("active");
+      activeCircle.next().addClass("active");
+    }
+  }
+
+  function prevImage(){
+    var activeImage = $(".images img.active");
+
+    var activeCircle = $(".nav i.active");
+
+    activeImage.removeClass("active");
+    activeCircle.removeClass("active");
+
+    if (activeImage.hasClass("first")) {
+      $(".images img.last").addClass("active");
+      $(".nav i.last").addClass("active");
+    }else {
+      activeImage.prev().addClass("active");
+      activeCircle.prev().addClass("active");
+    }
+  }
+
+});
